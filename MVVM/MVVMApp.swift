c0ba5a529620e1212c83988affb6547acd3f31a5
Dylan CAETANO <dcaetano8@icloud.com>
2025-12-29 20:19:02 +0100

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Dylan caetano on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
